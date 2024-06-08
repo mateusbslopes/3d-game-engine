de1af26eb4e97fc8a9409be341d6ca2af3b9072a
Mateus Lopes <mateusbslopes@gmail.com>
2024-06-08 13:37:36 -0300

    refactor: create separated funcion for app info

--- main.cpp
+++ main.cpp
@@ -91,13 +91,7 @@ private:
         return true;
     }
 
-    void createInstance()
-    {
-        if (enableValidationLayers && !checkValidationLayerSupport())
-        {
-            throw std::runtime_error("Validation layers requested but not available.");
-        }
-
+    VkApplicationInfo createAppInfo() {
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "3d Engine";
@@ -105,7 +99,17 @@ private:
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_0;
+        return appInfo;
+    }
 
+    void createInstance()
+    {
+        if (enableValidationLayers && !checkValidationLayerSupport())
+        {
+            throw std::runtime_error("Validation layers requested but not available.");
+        }
+
+        VkApplicationInfo appInfo = createAppInfo();
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
